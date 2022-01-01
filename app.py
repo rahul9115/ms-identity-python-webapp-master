0fa291316a1cc952827d70381329c7d9ea59e703
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2022-01-01 16:42:41 +0530

    intial

--- app.py
+++ app.py
@@ -35,7 +35,15 @@ def search():
     name=session["user"].get("name")
     
     return render_template("search_colab.html", user=(name.split(" "))[0], version=msal.__version__)
-
+@app.route("/crud")
+def crud():
+    if not session.get("user"):
+       session["flow"] = _build_auth_code_flow(scopes=app_config.SCOPE) 
+       return render_template("login.html", auth_url=session["flow"]["auth_uri"], version=msal.__version__)
+    print("Session user details of",session["user"])
+    name=session["user"].get("name")
+    
+    return render_template("crud_colab.html", user=(name.split(" "))[0], version=msal.__version__)
 
 
 @app.route("/login")
